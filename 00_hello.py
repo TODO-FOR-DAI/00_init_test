61a75f8685f9c0224a2579245537f3473195d2bb
DAI <daikill5963@gmail.com>
2023-03-05 23:31:41 +0900

    Readme 추가

--- 00_hello.py
+++ 00_hello.py
@@ -62,3 +62,81 @@ dic.clear()
 print(dic)
 
 #---------- Push to the Github
+
+s1 = set([1, 2, 3])
+s2={1, 2, 3}
+print(s1)
+print(s2)
+
+l = [1, 2, 2, 3, 3]
+newList = list(set(l))      # 중복이 제거된 set 을 만들고 이것을 다시 list 로 만들어 버림
+print(newList)
+
+s1 = set("hello")   # 집합은 순서가 없고, 중복이 없다
+print(s1)
+
+
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+print(s1 & s2)
+
+print(s1.intersection(s2))
+
+print(s1 | s2)
+
+print(s1 - s2)
+print(s2 - s1)
+
+s1.add(7)
+print(s1)
+
+s1.update([8, 9,1])
+print(s1)
+
+print(True)
+
+a = True
+if a:
+    print(a)
+
+b = False
+if b:
+    print("test")
+    print("block")
+else:
+    print("here")
+    print("se")
+
+
+
+a=[1, 2, 3]
+b= a
+b[1] = 4
+
+print(a)
+print(b)
+
+
+print(id(a))
+print(id(b))
+
+b = a[:]    # slicing 해서 새로운 객체를 만들어서 집어넣게 된다
+
+
+b[1] = 7
+print(a)
+print(b)
+
+
+print(id(a))
+print(id(b))
+
+
+a, b = ('pyhton', 'life')
+
+print(b)
+print(a)
+
+print(a, b)
+
+
---
+++ Readme.txt
@@ -0,0 +1,26 @@
+0. 참고 
+https://pythontutor.com/python-debugger.html#mode=edit
+
+
+1.  파이썬 입문강의
+https://www.youtube.com/watch?v=KL1MIuBfWe0&t=3427s
+
+2. 비트코인 투자 자동화
+https://www.youtube.com/watch?v=WgXOFtDD6XU&list=PLU9-uwewPMe3KKFMiIm41D5Nzx_fx2PUJ
+
+3. 주식투자 자동화
+https://www.youtube.com/watch?v=Y01D2J_7894&list=PLU9-uwewPMe0fB60VIMuKFV7gPDXmyOzp
+
+4. github
+https://www.youtube.com/watch?v=lelVripbt2M
+https://www.youtube.com/watch?v=cwC8t9dno2s
+https://www.youtube.com/watch?v=EjIncNR1k4s
+
+git 설명
+https://www.git-scm.com/book/ko/v2/%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-Git-%EC%B5%9C%EC%B4%88-%EC%84%A4%EC%A0%95
+
+환경변수 설정
+https://blog.naver.com/haven2216/222666760484
+
+git 주소... daikill5963@gmail.com	@kkyssh13GI
+git@github.com:TODO-FOR-DAI/02_company.git
